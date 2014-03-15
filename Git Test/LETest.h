45b9cace66aaf8fa621f01b1ce0773ff16cd3d3c
Leo Eddy <leoeddy@Leo-Eddys-iMac.local>
2014-03-15 01:16:55 -0700

    added a test class

---
+++ Git Test/LETest.h
@@ -0,0 +1,13 @@
+//
+//  LETest.h
+//  Git Test
+//
+//  Created by  Leo Eddy on 2014-03-15.
+//  Copyright (c) 2014  Leo Eddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LETest : NSObject
+
+@end
---
+++ Git Test/LETest.m
@@ -0,0 +1,13 @@
+//
+//  LETest.m
+//  Git Test
+//
+//  Created by  Leo Eddy on 2014-03-15.
+//  Copyright (c) 2014  Leo Eddy. All rights reserved.
+//
+
+#import "LETest.h"
+
+@implementation LETest
+
+@end
